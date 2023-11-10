eaa98b48d8f95339bc2fa5e184aab2a61654b776
Marcell O <omarcell@yahoo.com>
2023-11-10 19:27:59 +0000

    Modified gitignore, moved models

--- junction/main.py
+++ junction/main.py
@@ -3,8 +3,9 @@ import config as cfg
 
 modelPath = cfg.models["llama-2-7B"]
 llm = Llama(model_path=modelPath)
-
 prompt = "Q: Name the planets in the solar system? A:"
 
-output = llm(prompt , max_tokens=256, stop=["Q:", "\n"], echo=True, n_gpu_layers=-1)
-print(output)
+for word in  llm.create_completion(prompt, max_tokens=256, stop=["Q:", "\n"], echo=True, stream=True):
+    print(word)
+
+a = 1
