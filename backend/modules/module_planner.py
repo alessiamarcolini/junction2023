de935a46ddbf19170611462c6d2901d04ac1bfaf
Marcell O <omarcell@yahoo.com>
2023-11-10 22:55:56 +0000

    Planner implementation, custom prompt

--- backend/modules/module_planner.py
+++ backend/modules/module_planner.py
@@ -3,6 +3,41 @@ from .module_base import ModuleBase
 from llama_cpp import Llama
 import config as cfg
 
+CUSTOM_PROMPT = """
+You are a trustworthy assistant breaking down complex problems
+into easy-to-solve chunks. You have the following trusted models
+at your disposal:
+
+energy_price_forecast model:
+- parameters: 
+    - months: float
+
+steel_price_forecast model:
+- parameters: 
+    - months: float
+    - alloy: string
+
+Please use the above models to break down the problem given at 
+the end. Please respond in json format with the model name and
+model parameters and include your reasoning. If these models cannot
+solve the problem, please say "I cannot solve the problem".
+
+Here is one example to help:
+Input: What is the expected development of stainless steel 
+market pricing for 10B general steel alloy in two weeks?
+Output:
+[
+    {"modelName": "steel_price_forecast",
+    "parameters": {
+        "months": 0.5,
+        "alloy": "10B general steel"
+        }
+    }
+]
+
+Output:
+"""
+
 
 class PlannerModule(ModuleBase):
     def __init__(self, 
@@ -12,30 +47,32 @@ class PlannerModule(ModuleBase):
         self.modelName = modelName
         self.maxOutputTokens = 2048
         self.stream = False
+        self.temperature = 0.1
+        self.response_format = "json_object"
 
     def execute(self, handler: ModelHandler):
         self.__modelHandler = handler
         messages = handler.messages()
 
+        # Add custom prompt to beginning of
+        # last message
+        messages[-1] = CUSTOM_PROMPT + messages[-1]
 
-        handler.send_text("Hello world")
-        handler.send_image("https://i.imgur.com/2X2IDEA.jpeg")
-        handler.finalize()
-
-        pass
-
-    def generate_response(self):
-        wordGenerator = self.__model.create_chat_completion(
-            self.__modelHandler.messages(),
+        # Get latest prompt + respond
+        response = self.__model.create_chat_completion(
+            messages,
             max_tokens=self.maxOutputTokens,
-            stop=self.stopCharacters,
-            stream=self.stream
-            )
-        for word in wordGenerator:
-            self.__modelHandler.send_text(word["choices"][0]["text"])
+            stream=self.stream,
+            temperature=self.temperature,
+            response_format=self.response_format
+        )
+
+        # Try to make sense of the response
+        a = 1
         
         # TODO - remove
         self.__modelHandler.finalize()
 
 if __name__ == "__main__":
-    LlamaModule().generate_response("I want to help my friend generate pictures of cats using AI. Can you suggest some python code to achieve this?")
+    PlannerModule().execute("""I want to help my friend generate pictures of cats using AI.
+        Can you suggest some python code to achieve this?""")
