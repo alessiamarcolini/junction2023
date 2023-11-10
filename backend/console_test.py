2465a872c176aacf195f3c7bf71d23960b36bbc6
Bendeguz Cseresnyes <cser.bende@gmail.com>
2023-11-10 20:52:49 +0100

    Add module separation

--- backend/console_test.py
+++ backend/console_test.py
@@ -1,10 +1,13 @@
-from llama_module import LlamaModule
 from model_handler import ConsoleModelHandler
-
+import sys
 
 def main():
+    module_name = sys.argv[1]
+    module_import = __import__(module_name, fromlist=["Module"])
+
+    module = module_import.Module()
+
     print("Starting app in console mode")
-    module = LlamaModule()
     handler = ConsoleModelHandler()
     module.execute(handler)
     print("App finished")
