1535a4af8be3b175fcfd10a34e5fe6b214520b24
Marcell O <omarcell@yahoo.com>
2023-11-11 00:41:32 +0000

    Typing and tester script

---
+++ tester.py
@@ -0,0 +1,5 @@
+from backend.modules.module_planner import PlannerModule
+from backend.model_handler.console_model_handler import ConsoleModelHandler
+
+PlannerModule().execute(ConsoleModelHandler())
+# I want to know the price of steel in 6 weeks' time. Can you help me?
