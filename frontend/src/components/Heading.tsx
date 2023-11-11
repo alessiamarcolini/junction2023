043a7487d7cc40e13a5a3a17e0d60716239c0e05
Attila Makai <sandorattilamakai@gmail.com>
2023-11-11 22:08:38 +0200

    added logo

--- frontend/src/components/Heading.tsx
+++ frontend/src/components/Heading.tsx
@@ -1,8 +1,8 @@
 export const Heading = () => {
   return (
-    <div className='grid w-full justify-items-center'>
-      <img className="h-20 p-4"
-        src="https://otke-cdn.outokumpu.com/-/media/feature/identity/medium_horizontal_blue_rgb.png?h=143&w=561&revision=fbc0328f-0be5-43bd-81a3-8100c0258ffa&modified=20190916043102&webp=true"
+    <div className=''>
+      <img className="h-full max-h-24 p-2"
+        src="/logo.png"
         alt=""
       />
     </div>
