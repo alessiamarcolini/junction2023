d972b8324a525be07e94b939b6c7d0bb14ef1301
Marcell O <omarcell@yahoo.com>
2023-11-11 17:21:27 +0000

    pydantic removal

--- backend/custom_types/orchestration_types.py
+++ backend/custom_types/orchestration_types.py
@@ -1,10 +1,9 @@
 from typing import Any, List, Optional, Dict, Union
 from typing_extensions import TypedDict, NotRequired, Literal
-from pydantic import BaseModel
 
 from custom_types.message_types import ChatCompletionRequestMessage
 
-class OrhestrationPlan(BaseModel):
+class OrhestrationPlan(TypedDict):
     goal: Literal["deny", "explain"]
     reasoning: str
     relevantModels: NotRequired[Literal["ENERGY PRICE FORECAST MODEL", "STEEL PRICE FORECAST MODEL"]]
