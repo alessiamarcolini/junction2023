6370d7f6fa86e1809d0cf82ad84552032b6c6eca
Marcell O <omarcell@yahoo.com>
2023-11-11 19:43:42 +0000

    Save work commit

--- backend/orchestrators/orchestrator.py
+++ backend/orchestrators/orchestrator.py
@@ -1,8 +1,14 @@
+# Base packages
 import logging
 
+# Own packages
 from orchestrators.orchestrator_base import OrchestratorBase
 from model_handler.model_handler import ModelHandler
 from modules.module_planner import PlannerModule
+from modules.module_time import TimeModule
+
+# 3rd party packages
+import numpy as np
 
 class Orchestrator(OrchestratorBase):
     def __init__(self):
@@ -22,10 +28,23 @@ class Orchestrator(OrchestratorBase):
 
         # We have stuff to do
         if goal == "explain":
+            handler.update_status_message("Preparing forecasts...")
             models = orchestrationPlan["orchestrationPlan"]["relevantModels"]
             logging.info(f"Explaining model output using models: {models}")
 
             # Get timeline for predictions
-        
+            days = TimeModule(
+                modelName="mistral-7B-instruct"
+            )
+
+            # Convert days to months and round days/months
+            months = np.ceil(days/30)
+            days = np.ceil(days)
+
+            # Call-out to energy module relevant modules
+            moduleResults = {}
+            if "ENERGY PRICE FORECAST MODEL" in models:
+                handler.update_status_message(f"Running energy forecast for {days} days")
+                
         # Update progress bar for deny request
         handler.update_progress_bar(50)
