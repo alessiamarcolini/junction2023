734a16dd33687b627a66f8b2a366f160b87ad450
Bendeguz Cseresnyes <cser.bende@gmail.com>
2023-11-11 14:38:56 +0100

    Fix frontend serving from bff

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { SocketContext } from "./hooks/useSocket.ts";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("/");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
