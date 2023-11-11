e2f5a65dffad940ae3f151098f4842261b7da8b5
Marcell O <omarcell@yahoo.com>
2023-11-11 21:45:07 +0000

    Working streaming

--- backend/modules/module_deny.py
+++ backend/modules/module_deny.py
@@ -1,5 +1,6 @@
 # Base packages
 import copy
+import json
 
 # Custom packages
 from .module_base import ModuleBase
@@ -72,9 +73,16 @@ Input: {userPrompt}
 Output: 
 """
 
-class TimeModule(ModuleBase):
+TEMPLATES = {
+    "irrelevant": DENY_IRRELEVANT_PROMPT,
+    "no_model": DENY_NO_MODEL_PROMPT
+}
+
+class DenyModule(ModuleBase):
     def __init__(self, 
-        modelName: str = "mistral-7B-instruct"):
+        modelName: str = "mistral-7B-instruct"
+        ):
+
         super().__init__()
         self.maxOutputTokens = 1024
         self.temperature = 0.2
@@ -82,60 +90,43 @@ class TimeModule(ModuleBase):
         self.n_gpu_layers = 40
         self.n_batch = 512
         self.modelName = modelName
-        self.stream = False
-        self.__maxRetries = 3
+        self.stream = True
         self.__model = Llama(model_path=cfg.models[modelName], n_gpu_layers=128, n_ctx=self.maxOutputTokens)
 
         logging.info(f"Initialized deny response model {modelName}")
     
     def execute(
-        self, 
+        self,
         handler: ModelHandler, 
-        reason: str["irrelevant", "no_model"]) -> str:
+        reason: str) -> None:
 
-        logging.info("Executing deny response function")
+        logging.info(f"Executing deny response function for reason {reason}")
         self.__modelHandler = handler
         messages = handler.messages()
         tempMessages = copy.deepcopy(messages)
 
         # Get latest prompt + respond
-        timePrompt = PromptTemplate(template=DENY_NO_MODEL_PROMPT, input_variables=["userPrompt"])
-        timePrompt = timePrompt.format(userPrompt=messages[-1]["content"])
-        logging.info(f"Prompting time extraction model with: {timePrompt}")
-        tempMessages[-1]["content"] = timePrompt
+        prompt = PromptTemplate(template=TEMPLATES[reason], input_variables=["userPrompt"])
+        prompt = prompt.format(userPrompt=messages[-1]["content"])
+        logging.info(f"Prompting deny response model with: {prompt}")
+        tempMessages[-1]["content"] = prompt
 
-        # Task 1 - what is the timeframe of the request (in days)?
-        for i in range(self.__maxRetries):
-            filterResponse = self.__model.create_chat_completion(
-                tempMessages,
-                max_tokens=128,
-                stream=self.stream,
-                temperature=self.temperature,
-            )
-            responseText = filterResponse['choices'][0]['message']['content']
-            logging.info(f"Time estimation response {i} generated: {responseText}")
+        # Generate deny
+        responseStream = self.__model.create_chat_completion(
+            tempMessages,
+            max_tokens=1024,
+            stream=self.stream,
+            temperature=self.temperature,
+        )
+        result = []
+        for output in responseStream:
+            if output["choices"]["finish_reason"] is None:
+                try:
+                    word = output["choices"]["delta"]["content"]
+                except KeyError as e:
+                    logging.info(f"Key error encountered for model output stream {e}")
+                
+                handler.send_text(word)
+                result.append(word)
 
-            try:
-                timeResponse = json.loads(responseText)
-            except json.decoder.JSONDecodeError:
-                logging.warn(f"Time estimation model generated invalid JSON: {responseText}")
-                continue
-
-            try:
-                estimatedTime = int(timeResponse["days"])
-                timeResponse["reasoning"]
-                break
-            except ValueError as e:
-                logging.info(f"Exception encountered when validating JSON contents: {e}")
-            except KeyError as e:
-                logging.info(f"Generated response did not have all required keys: {e}")
-
-            
-            logging.info(f"Invalid time estimation model response. Retrying ... ({i}/{self.__maxRetries})")
-
-            if i + 1 == self.__maxRetries:
-                raise RuntimeError("No usable response from LLM model")
-        
-        # Return: Question not relevant
-        handler.send_debug_thoughts(timeResponse["reasoning"])
-        return timeResponse["days"]
+        logging.info(f"Deny response generated: {result}")
--- backend/tester.py
+++ backend/tester.py
@@ -1,5 +1,10 @@
-from modules.module_time import TimeModule
+from modules.module_deny import DenyModule
 from model_handler.console_model_handler import ConsoleModelHandler
 
-TimeModule("mistral-7B-instruct").execute(ConsoleModelHandler())
+DenyModule(
+    modelName="mistral-7B-instruct"
+    ).execute(
+        handler=ConsoleModelHandler(),
+        reason="no_model"
+        )
 # I'm going to Paris next week and I need to produce a time forecast for the energy prices for the next 3 months. Can you help me do that?
