5a9a95a66cabca7e87ef3b0fc5eb4f82f3c53615
Attila Makai <sandorattilamakai@gmail.com>
2023-11-11 11:02:20 +0200

    added logo, changed background and mobile scaling

---
+++ frontend/src/components/Heading.tsx
@@ -0,0 +1,10 @@
+export const Heading = () => {
+  return (
+    <div className='grid w-full justify-items-center'>
+      <img className="h-20 p-4"
+        src="https://otke-cdn.outokumpu.com/-/media/feature/identity/medium_horizontal_blue_rgb.png?h=143&w=561&revision=fbc0328f-0be5-43bd-81a3-8100c0258ffa&modified=20190916043102&webp=true"
+        alt=""
+      />
+    </div>
+  );
+};
