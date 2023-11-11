e6fd956c6d8b5f554a2d5736dbcedff659b85d39
alessiamarcolini <a.marcolini@lumicks.com>
2023-11-11 14:22:06 +0200

    Add dummy orchestrator to run energy model

---
+++ backend/orchestrators/orchestrator_dummy_energy.py
@@ -0,0 +1,24 @@
+import time
+
+from model_handler.model_handler import ModelHandler
+from modules.module_energy import EnergyModule
+from orchestrators.orchestrator_base import OrchestratorBase
+
+
+class Orchestrator(OrchestratorBase):
+    def __init__(self):
+        super().__init__()
+
+        self.energy_module = EnergyModule(model_name="WTF")
+
+    def execute(self, handler: ModelHandler):
+        handler.update_status_message("started")
+        time.sleep(2)
+        horizon = 7
+        handler.update_status_message(f"run energy module, horizon {horizon}")
+        handler.update_progress_bar(0)
+        energy_predictions = self.energy_module.execute(horizon=horizon)
+        handler.send_text(
+            f"Energy prediction for horizon {horizon}: {energy_predictions}"
+        )
+        handler.finalize()
