8a35c781c1fb79e0ab55b4e4b5df4b64ccc32ce8
Attila Makai <sandorattilamakai@gmail.com>
2023-11-11 18:46:13 +0200

    panic

--- frontend/src/components/MessageContainer.tsx
+++ frontend/src/components/MessageContainer.tsx
@@ -1,78 +1,19 @@
-import { useState } from "react";
 import { Message } from "../Messages";
-import { Overlay } from "./Overlay";
-import { Modal } from "./Modal";
+import { SystemMessageContainer } from "./SystemMessageContainer";
+import { UserMessageContainer } from "./UserMessageContainer";
 
 interface MessageContainerProps {
   message: Message;
-  hideDecision?: boolean
+  hideDecision?: boolean;
 }
 
-export const MessageContainer = ({ message, hideDecision = false }: MessageContainerProps) => {
-  const [currentOverlay, setCurrentOverlay] = useState<string>("");
-  const [showOverlay, setShowOverlay] = useState<boolean>(false);
-  const [showModal, setShowModal] = useState<boolean>(false);
-  return (
-    <div
-      className={`${
-        message.owner === "system" ? "justify-items-start" : "justify-items-end"
-      } grid  h-fit w-full`}
-    >
-      <div className="flex items-end">
-        {message.owner === "system" && (
-          <img
-            className="w-10 h-10 aspect-square"
-            src="https://ia.leadoo.com/upload/images/bot_icon/WTYxNb0TNUeTcKL9.png"
-            alt=""
-          />
-        )}
-        <div
-          className={`${
-            message.owner === "system"
-              ? "bg-secondary-100 shadow-secondary-200 text-secondary-300 mr-16"
-              : "bg-secondary-200 shadow-secondary-300 text-secondary-100 ml-16"
-          } rounded-xl p-4 m-4 shadow-lg`}
-        >
-          {message.fragments.map((fragment, fragmentIdx) =>
-            fragment.type === "text" ? (
-              <div key={fragmentIdx}>{fragment.text}</div>
-            ) : (
-              <div
-                key={fragmentIdx}
-                className="w-4/5 py-12 mx-auto"
-                onClick={() => {
-                  setCurrentOverlay(fragment.src);
-                  setShowOverlay(true);
-                }}
-              >
-                <img src={fragment.src} alt={fragment.src} />
-              </div>
-            ),
-          )}
-          {message.owner === "system" && !hideDecision && (
-            <button
-              onClick={() => setShowModal(true)}
-              className="text-secondary-300 rounded-xl border-2 border-secondary-300 hover:opacity-200 p-2 mt-4 text-xs"
-            >
-              Show thought process
-            </button>
-          )}
-        </div>
-      </div>
-      {showModal && (
-        <Modal
-          callback={() => {
-            setShowModal(false);
-          }}
-        >
-        {message.decision.map((d, idx) => 
-          <div key={idx}>{d}</div>
-        )}
-        </Modal>
-      )}
-      {showOverlay && (
-        <Overlay src={currentOverlay} callback={() => setShowOverlay(false)} />
-      )}
-    </div>
+export const MessageContainer = ({
+  message,
+  hideDecision = false,
+}: MessageContainerProps) => {
+  return message.owner === "system" ? (
+    <SystemMessageContainer message={message} hideDecision={hideDecision} />
+  ) : (
+    <UserMessageContainer message={message} />
   );
 };
--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,5 +1,7 @@
 export const RECOMMENDED_DEMO_MESSAGES = [
-  'Hey, this is a great question',
-  'This is an even better question',
-  'You could not come up with a better question than this even if the whole world depends on it'
-]
+  "Hey, this is a great question",
+  "This is an even better question",
+  "You could not come up with a better question than this even if the whole world depends on it",
+];
+
+export const STATIC_PATH = "/outfiles/";
