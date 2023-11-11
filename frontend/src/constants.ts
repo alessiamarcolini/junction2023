3cbdc28a96c7aafbaa87f78762a8228b9f815123
Attila Makai <sandorattilamakai@gmail.com>
2023-11-11 11:32:44 +0200

    added recommended conversation starter messages

---
+++ frontend/src/constants.ts
@@ -0,0 +1,5 @@
+export const RECOMMENDED_DEMO_MESSAGES = [
+  'Hey, this is a great question',
+  'This is an even better question',
+  'You could not come up with a better question than this even if the whole world depends on it'
+]
